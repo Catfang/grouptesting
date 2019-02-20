f7aefbb824d3d731d72791d6237ac046aeadf7b0
Catfang <catfang@gmail.com>
2019-02-20 11:38:07 -0700

    The game has been moved to its own class. This is the first step in "abstracting" the game from the UI. The next step will be to remove all references to the console from the game class.

---
+++ TheGuessingGame/TheGuessingGame/GuessingGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGuessingGame
+{
+    public class GuessingGame
+    {
+        public void Play()
+        {
+            // DEFINE WINNING GAME STATE
+            string inputLine = "";
+            bool stillPlaying = true;
+
+            while (stillPlaying)
+            {
+
+                Console.Clear();
+                // Made the win state Random! 
+                // Create a new random instance.
+                Random random = new Random();
+                // create an integer set it equal to a new random number with min and max bounds.  the min is inclusive, and the max is exclusive.
+                int answer = random.Next(1, 11);
+
+
+                // UI ELEMENT - Let's introduce this program!
+                Console.WriteLine("This is a guessing game. Please guess a number from 1 to 10!");
+
+                // USER INPUT
+                inputLine = Console.ReadLine();
+
+                // Convert our answer to a string
+                int guess = int.Parse(inputLine);
+
+                // Decide if our user has won
+                if (guess == answer) { Console.WriteLine("Congratulations! You won!"); }
+                else { Console.WriteLine("Ha ha ha! You loser!"); }
+
+                // Find out if player wants to play again after being called a loser
+                Console.WriteLine("Would you like to play again?");
+                inputLine = Console.ReadLine();
+                if (inputLine == "n" || inputLine == "N") { stillPlaying = false; }
+
+            }
+        }
+    }
+}
--- TheGuessingGame/TheGuessingGame/Program.cs
+++ TheGuessingGame/TheGuessingGame/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Dynamic;
 
 
 namespace TheGuessingGame
@@ -10,40 +11,11 @@ namespace TheGuessingGame
             // This *very* buggy and brittle code is the start of our first
             // group-driven game development.
 
-            // DEFINE WINNING GAME STATE
-            string inputLine = "";
-            bool stillPlaying = true;
+            //Create GuessingGame instance
+            GuessingGame game = new GuessingGame();
 
-            while (stillPlaying)
-            {
+            game.Play();
 
-                Console.Clear();
-                // Made the win state Random! 
-                // Create a new random instance.
-                Random random = new Random();
-                // create an integer set it equal to a new random number with min and max bounds.  the min is inclusive, and the max is exclusive.
-                int answer = random.Next(1, 11);
-
-
-                // UI ELEMENT - Let's introduce this program!
-                Console.WriteLine("This is a guessing game. Please guess a number from 1 to 10!");
-
-                // USER INPUT
-                inputLine = Console.ReadLine();
-
-                // Convert our answer to a string
-                int guess = int.Parse(inputLine);
-
-                // Decide if our user has won
-                if (guess == answer) { Console.WriteLine("Congratulations! You won!"); }
-                else { Console.WriteLine("Ha ha ha! You loser!"); }
-
-                // Find out if player wants to play again after being called a loser
-                Console.WriteLine("Would you like to play again?");
-                inputLine = Console.ReadLine();
-                if(inputLine == "n" || inputLine == "N") { stillPlaying = false; }
-                       
-            }
             // Added exit to make it an instant exit instead of a multiple key press.
             Environment.Exit(0);
 
